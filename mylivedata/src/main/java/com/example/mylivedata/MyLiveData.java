e0dc4d7e33ffe2e6c1c10d9ccbd0e22bbc0e1968
persiKey <parhomchuk308@gmail.com>
2024-07-16 13:36:04 +0300

    created a draft of mylivedata

--- mylivedata/src/main/java/com/example/mylivedata/MyLiveData.java
+++ mylivedata/src/main/java/com/example/mylivedata/MyLiveData.java
@@ -1,4 +1,33 @@
 package com.example.mylivedata;
 
-public class MyLiveData {
+import androidx.annotation.MainThread;
+import androidx.lifecycle.Observer;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MyLiveData<T> extends Object {
+    private T data;
+    private List<Observer<T>> observers;
+
+
+    public MyLiveData(T data) {
+        this.data = data;
+        observers = new ArrayList<>();
+    }
+
+    public void setData(T data) {
+        this.data = data;
+        for (Observer<T> observer : observers) {
+            observer.onChanged(this.data);
+        }
+    }
+
+    public T getData() {
+        return data;
+    }
+
+    void observe(Observer<T> observer) {
+        observers.add(observer);
+    }
 }
